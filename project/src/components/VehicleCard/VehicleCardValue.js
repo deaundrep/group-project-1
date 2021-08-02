91993bcb9d348b4711acf242525fe0761383af80
fernando duenas <fernando39615@aim.com>
2021-08-02 11:15:32 -0400

    accepting changes

--- project/src/components/VehicleCard/VehicleCardValue.js
+++ project/src/components/VehicleCard/VehicleCardValue.js
@@ -1,98 +1,92 @@
-import React, { useState, useRef, useEffect } from "react";
-import "./VehicleCard.css";
+import React, { useState, useRef, useEffect } from 'react'
+import './vehicleCard.css'
 
-export default function VehicleCardValue(props) {
-	const [editBtn, setEditBtn] = useState(false);
-	const [vehicleCardState, setVehicleCardState] = useState({
-		make: props.make,
-		model: props.model,
-		price: props.price,
-		id: props.id,
-	});
-	const [disableBtn, setDisableBtn] = useState(false);
+export default function vehicleCard(props) {
 
-	const onChangeHandler = (e) => {
-		setVehicleCardState({
-			...vehicleCardState,
-			[e.target.name]: e.target.value,
-		});
-	};
+    const [editBtn, setEditBtn] = useState(false)
+    const [vehicleCardState, setVehicleCardState] = useState({
+        make: props.make,
+        model: props.model,
+        price: props.price,
+        id: props.id
+    })
+    const [disableBtn, setDisableBtn] = useState(false)
 
-	useEffect(() => {
-		vehicleCardState.make === "" ||
-		vehicleCardState.model === "" ||
-		vehicleCardState.price === ""
-			? setDisableBtn(true)
-			: setDisableBtn(false);
-	}, [vehicleCardState]);
+    const onChangeHandler = (e) => {
+        setVehicleCardState({
+            ...vehicleCardState,
+            [e.target.name]: e.target.value
+        })
+    }
 
-	const editVehicleCard = (e) => {
-		e.preventDefault();
-		let editVehicleObj = {
-			make: vehicleCardState.make,
-			model: vehicleCardState.model,
-			price: vehicleCardState.price,
-			id: vehicleCardState.id,
-		};
-		props.editVehicle(editVehicleObj);
-		setEditBtn(false);
-	};
+    useEffect(() => {
+        vehicleCardState.make === '' || vehicleCardState.model === '' || vehicleCardState.price === '' ?
+            setDisableBtn(true) : setDisableBtn(false)
+    }, [vehicleCardState])
 
-	return (
-		<div className="vehicle-card-div">
-			{editBtn ? (
-				<div>
-					<form onSubmit={editVehicleCard}>
-						<label htmlFor="make">make: </label>
-						<input
-							type="text"
-							name="make"
-							ref={props.editMakeRef}
-							value={vehicleCardState.make}
-							onChange={onChangeHandler}
-							placeholder={vehicleCardState.make}
-						/>
-						<br />
-						<label htmlFor="model">model: </label>
-						<input
-							type="text"
-							name="model"
-							ref={props.editModelRef}
-							value={vehicleCardState.model}
-							onChange={onChangeHandler}
-							placeholder={vehicleCardState.model}
-						/>
-						<br />
-						<label htmlFor="price">Price: </label>
-						<input
-							type="text"
-							name="price"
-							ref={props.editPriceRef}
-							value={vehicleCardState.price}
-							onChange={onChangeHandler}
-							placeholder={vehicleCardState.price}
-						/>
-						<br />
-						<button disabled={disableBtn} type="submit">
-							Submit Changes
-						</button>
-					</form>
-				</div>
-			) : (
-				<div>
-					<h2>{props.make}</h2>
-					<h2>Model: {props.model}</h2>
-					<p>Price: {props.price}</p>
-				</div>
-			)}
+    const editVehicleCard = (e) => {
+        e.preventDefault()
+        let editVehicleObj = {
+            make: vehicleCardState.make,
+            model: vehicleCardState.model,
+            price: vehicleCardState.price,
+            id: vehicleCardState.id
+        }
+        props.editVehicle(editVehicleObj)
+        setEditBtn(false)
+    }
 
-			<button onClick={() => props.addToCart(vehicleCardState)}>
-				Add to Cart
-			</button>
+    return (
+        <div className="vehicle-card-div">
 
-			<button onClick={() => setEditBtn((prev) => !prev)}>Edit</button>
+            {
+                editBtn ? (
+                    <div>
+                        <form onSubmit={editVehicleCard}>
+                            <label htmlFor="make">make: </label>
+                            <input
+                                type='text'
+                                name="make"
+                                // ref={editMakeRef}
+                                value={vehicleCardState.make}
+                                onChange={onChangeHandler}
+                                placeholder={vehicleCardState.make}
+                            /><br />
+                            <label htmlFor="model">model: </label>
+                            <input
+                                type='text'
+                                name="model"
+                                value={vehicleCardState.model}
+                                onChange={onChangeHandler}
+                                placeholder={vehicleCardState.model}
+                            /><br />
+                            <label htmlFor="price">Price: </label>
+                            <input
+                                type='text'
+                                name="price"
+                                value={vehicleCardState.price}
+                                onChange={onChangeHandler}
+                                placeholder={vehicleCardState.price}
+                            /><br />
+                            <button disabled={disableBtn} type='submit'>Submit Changes</button>
+                        </form>
+                    </div>
+                ) : (
+                    <div>
+                        <h2><u>{props.make}</u></h2>
+                        <p>model: {props.model}</p>
+                        <p>Price: {props.price}</p>
+                    </div>
+                )
+            }
 
-			<button onClick={() => props.deleteVehicle(props.id)}>Delete Me!</button>
-		</div>
-	);
+            <button onClick={() => props.addToCart(vehicleCardState)}>Add to Cart</button>
+
+            <button onClick={() => setEditBtn(prev => !prev)}>Edit</button>
+
+            <button
+                onClick={() => props.deleteVehicle(props.id)}
+            >Delete Me!</button>
+        </div>
+    )
 }
